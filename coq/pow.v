684e0a2411cbc3001dc528a184e537c2e5857ec1
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-09-30 09:23:14 +0200

    showed cps proof

--- coq/pow.v
+++ coq/pow.v
@@ -2,6 +2,8 @@ Require Import Coq.Numbers.NatInt.NZDiv.
 Require Import NPeano.
 Require Import Lia.
 Require Import Vector.
+Require Import FunInd.
+
 Import VectorNotations.
 
 Definition Idx (n:nat) := nat.
@@ -61,10 +63,54 @@ Definition cast {T:Type} (U:Type) (t:T) (p:T = U) : U :=
   | eq_refl => t
   end.
 
-Check pow.
 
 
 Section code_definitions.
+
+
+  Check pow.
+
+  Fixpoint pow_cps_ref (a b:nat) (ret:Cn nat) :=
+    match b with
+    | 0 => ret 1
+    | S b' => pow_cps_ref a b' (fun x => ret (a * x))
+    end.
+
+  Lemma pow_cps_congruence:
+    forall a b (ret1 ret2: Cn nat),
+      (forall x, ret1 x -> ret2 x) ->
+      pow_cps_ref a b ret1 -> pow_cps_ref a b ret2.
+  Proof.
+    intros a b ret1 ret2 H H1.
+    induction b in ret1, ret2, H, H1 |- *.
+    - simpl in *.
+      apply H.
+      assumption.
+    - simpl in *.
+      eapply IHb.
+      2: apply H1.
+      intros.
+      apply H.
+      apply H0.
+  Qed.
+
+
+  Goal forall a b, pow_cps_ref a b (fun c => c = pow a b).
+  Proof.
+    intros.
+    induction b.
+     (* in a |- *. *)
+    - simpl. reflexivity.
+    - simpl. 
+      eapply pow_cps_congruence.
+      2: apply IHb.
+      simpl.
+      lia.
+  Qed.
+
+    
+    rewrite IHb. reflexivity.
+
 
   Axiom (free_cast: forall {T:Type} {a b:T}, a=b).
 
@@ -123,7 +169,8 @@ Section code_definitions.
   Goal forall a b, f ((a,b),(fun _ => True)).
   Proof.
     intros.
-    (* unfold f. *)
+
+    unfold f.
     (* cbv [f]. *)
 
 
