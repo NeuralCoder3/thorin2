d1aedb52478e75508028790ce7077685f32ac2b0
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-11-14 10:50:15 +0100

    tests with autogen

--- dialects/rusty/rust_rusty/build.rs
+++ dialects/rusty/rust_rusty/build.rs
@@ -1,5 +1,10 @@
-fn main() {
-    let _build = cxx_build::bridge("src/lib.rs");
+fn main() -> miette::Result<()> {
+    // let _build = cxx_build::bridge("src/lib.rs");
+    let path = std::path::PathBuf::from("src");
+    let mut b = autocxx_build::Builder::new("src/lib.rs", [&path]).build()?;
+    // b.flag_if_supported("-std=c++14").compile("autocxx-demo");
+    b.flag_if_supported("-std=c++14").compile("rust_rusty");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
+    Ok(())
 }
--- dialects/rusty/rust_rusty/src/lib.rs
+++ dialects/rusty/rust_rusty/src/lib.rs
@@ -1,22 +1,64 @@
-// use std::fmt;
+use autocxx::prelude::*;
+use std::pin::Pin;
 
-#[cxx::bridge]
-mod ffi {
+// include_cpp! {
+//     #include "input.h"
+//     safety!(unsafe_ffi)
+//     generate!("jurassic")
+// }
 
-    #[namespace = "rust_rusty"]
-    extern "Rust" {
-        fn rust_echo(val: i32) -> i32;
-        fn debug(val: i32) -> ();
-    }
+// fn main() {
+//     ffi::jurassic();
+// }
+
+// #[autocxx::extern_rust::extern_rust_function]
+// pub fn new_dinosaur(carnivore: bool) -> Box<Dinosaur> {
+//     Box::new(Dinosaur { carnivore })
+// }
+
+
+// #[autocxx::extern_rust::extern_rust_type]
+// pub struct Dinosaur {
+//     carnivore: bool,
+// }
+
+// #[autocxx::extern_rust::extern_rust_function]
+// pub fn new_dinosaur(carnivore: bool) -> Box<Dinosaur> {
+//     Box::new(Dinosaur { carnivore })
+// }
+
+// impl Dinosaur {
+//     #[autocxx::extern_rust::extern_rust_function]
+//     fn roar(&self) {
+//         println!("Roar");
+//     }
+
+//     #[autocxx::extern_rust::extern_rust_function]
+//     fn eat(self: Pin<&mut Dinosaur>, other_dinosaur: Box<Dinosaur>) {
+//         assert!(self.carnivore);
+//         other_dinosaur.get_eaten();
+//         println!("Nom nom");
+//     }
+
+//     fn get_eaten(&self) {
+//         println!("Uh-oh");
+//     }
+// }
+
+// #[autocxx::extern_rust::extern_rust_function]
+// pub fn go_extinct() {
+//     println!("Boom")
+// }
+
+
+#[autocxx::extern_rust::extern_rust_function]
+fn debug(world: i32) -> () {
+    println!("Hello, world! {}", world);
+
 }
-
-
-fn debug(val: i32) -> () {
-    println!("debug: {}", val);
-}
-
 
 // #[inline(always)]
+#[autocxx::extern_rust::extern_rust_function]
 fn rust_echo(val: i32) -> i32 {
-    val+5
+    val+7
 }
