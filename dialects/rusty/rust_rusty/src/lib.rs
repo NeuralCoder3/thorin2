9588b79ec47a1a7ae32a015ec4cb76a6107b4784
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-11-14 10:49:04 +0100

    working bindcxx

--- dialects/rusty/rust_rusty/src/lib.rs
+++ dialects/rusty/rust_rusty/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+// use std::fmt;
 
 #[cxx::bridge]
 mod ffi {
@@ -6,11 +6,17 @@ mod ffi {
     #[namespace = "rust_rusty"]
     extern "Rust" {
         fn rust_echo(val: i32) -> i32;
+        fn debug(val: i32) -> ();
     }
 }
 
 
-#[inline(always)]
+fn debug(val: i32) -> () {
+    println!("debug: {}", val);
+}
+
+
+// #[inline(always)]
 fn rust_echo(val: i32) -> i32 {
     val+5
 }
