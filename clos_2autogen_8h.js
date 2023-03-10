471eb941851fa3899c31e2f4011010af5ea80ed6
NeuralCoder3 <NeuralCoder3@users.noreply.github.com>
2023-03-10 14:27:36 +0000

    deploy: 717fc70910a82364ae3bba66ea5be3b667a424c1

--- clos_2autogen_8h.js
+++ clos_2autogen_8h.js
@@ -9,11 +9,11 @@ var clos_2autogen_8h =
     [ "thorin::Axiom::Match< clos::lower_typed_clos_prep_pass >", "classthorin_1_1Axiom.html#structthorin_1_1Axiom_1_1Match_3_01clos_1_1lower__typed__clos__prep__pass_01_4", [
       [ "type", "classthorin_1_1Axiom.html#acfcc47bf4110b009071da2779ee35caf", null ]
     ] ],
-    [ "thorin::Axiom::Match< clos::clos2sjlj_pass >", "classthorin_1_1Axiom.html#structthorin_1_1Axiom_1_1Match_3_01clos_1_1clos2sjlj__pass_01_4", [
-      [ "type", "classthorin_1_1Axiom.html#a583701a1b6005be3e5b073398e113350", null ]
-    ] ],
     [ "thorin::Axiom::Match< clos::lower_typed_clos_pass >", "classthorin_1_1Axiom.html#structthorin_1_1Axiom_1_1Match_3_01clos_1_1lower__typed__clos__pass_01_4", [
       [ "type", "classthorin_1_1Axiom.html#aa3d4dfaca1c111efbba6934eab4aed9b", null ]
+    ] ],
+    [ "thorin::Axiom::Match< clos::clos2sjlj_pass >", "classthorin_1_1Axiom.html#structthorin_1_1Axiom_1_1Match_3_01clos_1_1clos2sjlj__pass_01_4", [
+      [ "type", "classthorin_1_1Axiom.html#a583701a1b6005be3e5b073398e113350", null ]
     ] ],
     [ "thorin::Axiom::Match< clos::branch_clos_pass >", "classthorin_1_1Axiom.html#structthorin_1_1Axiom_1_1Match_3_01clos_1_1branch__clos__pass_01_4", [
       [ "type", "classthorin_1_1Axiom.html#a30506ad337187cc6cf713c9bc72bc5a5", null ]
@@ -36,7 +36,7 @@ var clos_2autogen_8h =
     [ "lower_typed_clos_pass", "clos_2autogen_8h.html#ab52d3894a6198408374175b6fed51a48", null ],
     [ "lower_typed_clos_prep_pass", "clos_2autogen_8h.html#a1b7c65a97fcd27aead23ea5bbf794653", null ],
     [ "setjmp", "clos_2autogen_8h.html#a553c0b470a1ee42cdae9d1fd3006dec8", null ],
-    [ "normalize_clos", "clos_2autogen_8h.html#a55dab78e3533d57471a9a3c7399df6ce", null ],
+    [ "normalize_clos", "clos_2autogen_8h.html#ac9f972eb39228514383964979ae688b1", null ],
     [ "operator&", "clos_2autogen_8h.html#a6bdeb99fedfd82bcd5b30adfe470ef14", null ],
     [ "operator&", "clos_2autogen_8h.html#a1544fa4ef5d289049c75829425139504", null ],
     [ "operator==", "clos_2autogen_8h.html#aa999d58fd04f9e8281311fda55810a33", null ],
